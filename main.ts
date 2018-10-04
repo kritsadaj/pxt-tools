3c533980803cafc0d3ca835cedda0de35ca7f15d
kritsadaj <kritsadaj@gmail.com>
2018-10-04 19:32:05 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,11 @@
 //% weight=0 color=#ff00ff icon="\uf2a2" block="Encoder"
-namespace Encoder {
-    /**
-    * คำนวณพื้นที่สี่เหลี่ยม，มีการคืนค่า
-    */
-    //% blockId="areaOfRectangle" block="area of rectangle length %length|width %width"
-    //% blockGap=2 weight=0 blockExternalInputs=true
-    export function areaOfRectangle(length: number, width:number): number {
-        return length*width
-    }
-    /**
-    * คำนวณพื้นที่สี่เหลี่ยม，ไม่คืนค่า，แสดงผลที่ LED
-    */
-    //% blockId="ledOfRectangle" block="show area of rectangle length %length|width %width"
-    //% blockGap=2 weight=1
-    export function ledOfRectangle(length: number, width:number): void {
-        basic.showNumber(length*width)
-    }
+namespace encoder {
     /**
     *  ทดสอบ Encoder
     */
-    //% blockId="encoder" block="Encoder %Cnt"
+    //% blockId="encoders" block="Encoder %Cnt"
     //% blockGap=2 weight=1
-    export function encoder(Cnt: number): void {
+    export function encoders(Cnt: number): void {
         let x=0
         while(x<Cnt){
             while(pins.digitalReadPin(DigitalPin.P8==0){}
