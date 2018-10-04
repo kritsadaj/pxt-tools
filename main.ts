57a996c9f929ea702f5164f864072b13689108ce
kritsadaj <kritsadaj@gmail.com>
2018-10-04 19:45:17 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -8,10 +8,9 @@ namespace encoder {
     export function encoders(Cnt: number): void {
         let x=0
         while(x<Cnt){
-            while(pins.digitalReadPin(DigitalPin.P8==0){}
-            x +=1
-            while(pins.digitalReadPin(DigitalPin.P8==1){}
-            x +=1
+            while(pins.digitalReadPin(DigitalPin.P8==0)){}
+            x=x+1
+            while(pins.digitalReadPin(DigitalPin.P8==1)){}
         }
     }
 }
